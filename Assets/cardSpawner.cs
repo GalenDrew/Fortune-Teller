27ce71cbf0a2657f159c306041028f584a462ac8
GalenDrew <galenndrew@gmail.com>
2023-08-19 09:56:05 -0700

    utr

--- Assets/cardSpawner.cs
+++ Assets/cardSpawner.cs
@@ -39,7 +39,12 @@ public class cardSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
         {
             if (currentCardState == cardStates.preDraw)
